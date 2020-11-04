432ab709a23624dc683331d772757043db44cbdd
MathsPrado <matheusprado55@gmail.com>
2020-11-03 23:30:02 -0300

    Project
    Add Project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matheus Prado on 02/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
